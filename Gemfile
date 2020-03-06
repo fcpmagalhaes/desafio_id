3bca834e7de9dc29c91e6b46a2f799d871509d74
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-03-06 03:18:59 -0300

    Create devise and simple token autentication

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'pg', '~> 1.2', '>= 1.2.2'
 gem 'rack-cors'
 gem 'rack-attack'
+gem 'devise'
+gem 'simple_token_authentication', '~> 1.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,7 @@ module ApiDesafio
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*',
-          headers: :any,
-          methods: %i(get post put patch delete options head)
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
